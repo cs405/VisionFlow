16e2418fad7bbf10b27fcceeb586bd7a08db9b26
JKDCPPZzz <1908095603@qq.com>
2026-06-09 17:01:41 +0800

    边缘保留滤波

--- nodes/blurs/__init__.py
+++ nodes/blurs/__init__.py
@@ -1,4 +1,5 @@
 from nodes.blurs.blur import Blur
 from nodes.blurs.gaussian_blur import GaussianBlur
-from nodes.blurs.detail_enhance import DetailEnhance, EdgePreservingFilter
+from nodes.blurs.detail_enhance import DetailEnhance
+from nodes.blurs.edge_blur import EdgePreservingFilter
 from nodes.blurs.pencil_sketch import PencilSketch, Stylization
